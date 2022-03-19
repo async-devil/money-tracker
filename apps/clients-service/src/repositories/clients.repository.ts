6b7b4c3e02ae97f4d0c1851c72450f0ee7e801cf
async-devil <async.devil@gmail.com>
2022-03-19 19:06:40 +0200

    feat(clients-s): add clients repository

---
+++ apps/clients-service/src/repositories/clients.repository.ts
@@ -0,0 +1,47 @@
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { CreateClientDto } from "../dtos/create-client.dto";
+import { Client } from "../entities/client.entity";
+
+@Injectable()
+export class ClientsRepository {
+	constructor(@InjectRepository(Client) private readonly repository: Repository<Client>) {}
+
+	private throwDefaultError(message?: string) {
+		throw new InternalServerErrorException(message || "Internal Server Error");
+	}
+
+	public async getOneByCredential(credential: { name: string; value: unknown }): Promise<Client> {
+		let client: Client;
+
+		try {
+			client = await this.repository.findOne({ [`${credential.name}`]: credential.value });
+		} catch (err) {
+			this.throwDefaultError();
+		}
+
+		if (!client) throw new NotFoundException("Client not found");
+		return client;
+	}
+
+	public async create(dto: CreateClientDto): Promise<Client> {
+		try {
+			return await this.repository.save(dto);
+		} catch (err) {
+			const error = err as Error;
+
+			if (error.message.includes("duplicate key value violates unique constraint")) {
+				throw new BadRequestException("Duplicate client");
+			}
+
+			this.throwDefaultError();
+		}
+	}
+}
