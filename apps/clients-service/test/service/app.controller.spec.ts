0e8a4d97b8f205b730c6ce6782f9b3afa13800a2
async-devil <async.devil@gmail.com>
2022-03-19 19:11:07 +0200

    test(clients-s): add controller test

--- apps/clients-service/test/service/app.controller.spec.ts
+++ apps/clients-service/test/service/app.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
 import { AppController } from "src/app.controller";
 import { AppService } from "src/app.service";
+import { Client } from "src/entities/client.entity";
+import { ClientsRepository } from "src/repositories/clients.repository";
+
+import { ClientModel } from "../mocks/client.repository.mock";
 
 describe("AppController", () => {
 	let appController: AppController;
@@ -8,7 +14,14 @@ describe("AppController", () => {
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
 			controllers: [AppController],
-			providers: [AppService],
+			providers: [
+				AppService,
+				ClientsRepository,
+				{
+					provide: getRepositoryToken(Client),
+					useValue: ClientModel,
+				},
+			],
 		}).compile();
 
 		appController = app.get<AppController>(AppController);
