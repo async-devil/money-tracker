52f93670da2cfd626f8a29e333f22e9f6a38f9ad
async-devil <async.devil@gmail.com>
2022-03-20 23:53:40 +0200

    feat(clients-s): add clients repository

--- apps/clients-service/src/app.module.ts
+++ apps/clients-service/src/app.module.ts
@@ -5,10 +5,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { typeOrmConfigBase } from "./database/ormconfig";
 import { Client } from "./entities/client.entity";
+import { ClientsRepository } from "./repositories/clients.repository";
 
 @Module({
 	imports: [TypeOrmModule.forRoot(typeOrmConfigBase()), TypeOrmModule.forFeature([Client])],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, ClientsRepository],
 })
 export class AppModule {}
