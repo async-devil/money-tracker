f26807737cbd49c61c2ec297b2d4f9f0a4e3f873
async-devil <async.devil@gmail.com>
2022-03-23 21:52:51 +0200

    feat(auth-s): init typeorm

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { Client } from "@nestjs/microservices";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { typeOrmConfigBase } from "./database/ormconfig";
 
 @Module({
+	imports: [TypeOrmModule.forRoot(typeOrmConfigBase()), TypeOrmModule.forFeature([Client])],
 	controllers: [AppController],
 	providers: [AppService],
 })
