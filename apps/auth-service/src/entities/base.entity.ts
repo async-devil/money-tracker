33bb28eef4a0b58e0fad35f6b2b317e2830bf966
async-devil <async.devil@gmail.com>
2022-04-03 23:23:36 +0300

    feat(auth-s): update base entity

--- apps/auth-service/src/entities/base.entity.ts
+++ apps/auth-service/src/entities/base.entity.ts
@@ -1,15 +1,17 @@
-import { PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn } from "typeorm";
+import { UpdateDateColumn, CreateDateColumn } from "typeorm";
 
 export abstract class BaseEntity {
-	/** @example "123e4567-e89b-12d3-a456-426655440000" */
-	@PrimaryGeneratedColumn("uuid")
-	id: string;
+	/** @example "2016-06-22 19:10:25-07" */
+	@CreateDateColumn({
+		type: "timestamp with time zone",
+		default: () => "CURRENT_TIMESTAMP",
+	})
+	create_date_time: Date;
 
 	/** @example "2016-06-22 19:10:25-07" */
-	@CreateDateColumn({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
-	createDateTime: Date;
-
-	/** @example "2016-06-22 19:10:25-07" */
-	@UpdateDateColumn({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
-	lastChangedDateTime: Date;
+	@UpdateDateColumn({
+		type: "timestamp with time zone",
+		default: () => "CURRENT_TIMESTAMP",
+	})
+	last_changed_date_time: Date;
 }
