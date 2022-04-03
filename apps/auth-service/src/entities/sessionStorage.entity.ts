2d1a1b4114c3549fc46f5dd53e0983dd100b1de9
async-devil <async.devil@gmail.com>
2022-04-03 23:24:26 +0300

    feat(auth-s): add sessionStorage entity

---
+++ apps/auth-service/src/entities/sessionStorage.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, OneToMany, PrimaryColumn } from "typeorm";
+
+import { BaseEntity } from "./base.entity";
+import { Session } from "./session.entity";
+
+@Entity({ name: "session_storage" })
+export class SessionStorage extends BaseEntity {
+	/** @example "123e4567-e89b-12d3-a456-426655440000" */
+	@PrimaryColumn({ type: "varchar", unique: true })
+	client_id: string;
+
+	@OneToMany(() => Session, (session) => session.client_id)
+	sessions: Session[];
+}
