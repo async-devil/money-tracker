4c265cb7cfc451f2ad1a87acac436212494cb9f6
async-devil <async.devil@gmail.com>
2022-04-05 23:56:34 +0300

    feat(auth-s): add module dependencies

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from "@nestjs/common";
-import { Client } from "@nestjs/microservices";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { typeOrmConfigBase } from "./database/ormconfig";
+import { Session } from "./entities/session.entity";
+import { SessionStorage } from "./entities/sessionStorage.entity";
+import { SessionRepository } from "./repositories/session.repository";
+import { SessionStorageRepository } from "./repositories/sessionStorage.repository";
+import { AccessTokenService } from "./services/accessToken.service";
+import { RefreshTokenService } from "./services/refreshToken.service";
 
 @Module({
-	imports: [TypeOrmModule.forRoot(typeOrmConfigBase()), TypeOrmModule.forFeature([Client])],
+	imports: [
+		TypeOrmModule.forRoot(typeOrmConfigBase()),
+		TypeOrmModule.forFeature([Session, SessionStorage]),
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		AccessTokenService,
+		RefreshTokenService,
+		SessionRepository,
+		SessionStorageRepository,
+	],
 })
 export class AppModule {}
