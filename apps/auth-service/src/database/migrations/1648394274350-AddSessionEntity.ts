022c2f46229f27566644867994880e05af5543de
async-devil <async.devil@gmail.com>
2022-04-10 04:03:10 +0300

    feat(auth-s): update session entity

--- apps/auth-service/src/database/migrations/1648394274350-AddSessionEntity.ts
+++ apps/auth-service/src/database/migrations/1648394274350-AddSessionEntity.ts
@@ -9,6 +9,7 @@ export class AddSessionEntity1648394274350 implements MigrationInterface {
 			CREATE TABLE "session" (
 				"refresh_token"          character varying NOT NULL,
 				"client_id"              character varying NOT NULL,
+				"valid_until"            TIMESTAMP WITH TIME ZONE NOT NULL, 
 				"ip"                     inet NOT NULL,
 				"device"                 character varying NOT NULL,
 
@@ -18,20 +19,9 @@ export class AddSessionEntity1648394274350 implements MigrationInterface {
 				CONSTRAINT "PK_14f5d9fd42ee29c579807b5f7e5" PRIMARY KEY ("refresh_token")
 			)`
 		);
-		await queryRunner.query(
-			`
-			ALTER TABLE "session"
-			ADD CONSTRAINT "FK_49bf92b17e136d0bc8b9a915ab1" FOREIGN KEY ("client_id") REFERENCES "session_storage"("client_id")
-
-			ON DELETE NO ACTION
-			ON UPDATE NO ACTION`
-		);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(
-			`ALTER TABLE "session" DROP CONSTRAINT "FK_49bf92b17e136d0bc8b9a915ab1"`
-		);
 		await queryRunner.query(`DROP TABLE "session"`);
 	}
 }
--- apps/auth-service/src/entities/session.entity.ts
+++ apps/auth-service/src/entities/session.entity.ts
@@ -1,24 +1,25 @@
-import { Column, Entity, ManyToOne, JoinColumn, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 import { BaseEntity } from "./base.entity";
-import { SessionStorage } from "./sessionStorage.entity";
 
 @Entity({ name: "session" })
 export class Session extends BaseEntity {
-	/** @example "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDk0MTkzMDR9.GRnpWKUZmeAJvJzc4oIXceH1Bz3kyLHJBAx_pfG2iJg" */
-	@PrimaryColumn({ type: "varchar", nullable: false, unique: true })
+	/** @example "b4d72d7e7ca77bc8221f81083fc24c5ec11f3dfe446bf521" */
+	@PrimaryColumn({ type: "varchar", unique: true })
 	refresh_token: string;
 
 	/** @example "123e4567-e89b-12d3-a456-426655440000" */
-	@ManyToOne(() => SessionStorage)
-	@JoinColumn({ name: "client_id" })
+	@Column({ type: "varchar" })
 	client_id: string;
 
+	@Column({ type: "timestamp with time zone" })
+	valid_until: Date;
+
 	/** @example "57.37.103.24" */
-	@Column({ type: "inet", nullable: false })
+	@Column({ type: "inet" })
 	ip: string;
 
 	/** @example "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0)"*/
-	@Column({ type: "varchar", nullable: false })
+	@Column({ type: "varchar" })
 	device: string;
 }
