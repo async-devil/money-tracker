3ac335667f621babd49e78b069ce040c5bc0c507
async-devil <async.devil@gmail.com>
2022-04-21 23:53:41 +0300

    rearrange: delete server folder due to unusability

--- apps/server/scripts/writeOrmConfig.ts
+++
@@ -1,5 +0,0 @@
-import fs = require("fs");
-
-import { typeOrmConfig } from "../src/config/typeOrm.config";
-
-fs.writeFileSync("ormconfig.json", JSON.stringify(typeOrmConfig(), null, 2));
--- apps/server/test/clients/mocks/client.repository.mock.ts
+++
@@ -1,7 +0,0 @@
-import { clientStub } from "../stubs/client.stub";
-
-export const ClientModel = {
-	find: jest.fn(),
-	findOne: jest.fn(),
-	save: jest.fn(),
-};
