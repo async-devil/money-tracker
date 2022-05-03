acbb047bba7c736b210ddfc2fd744afa2bf2b9bb
async-devil <async.devil@gmail.com>
2022-05-03 19:42:13 +0300

    feat(gateway): add global ping method

--- apps/gateway/src/app.controller.ts
+++ apps/gateway/src/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 
-import { AppService } from "./app.service";
+import { AppService, PingResult } from "./app.service";
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
+
+	@Get("ping")
+	public async ping(): Promise<PingResult[]> {
+		return await this.appService.ping();
+	}
 }
--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -1,4 +1,66 @@
 import { Injectable } from "@nestjs/common";
+import { ApiProperty } from "@nestjs/swagger";
+
+import { AccountsService } from "./modules/accounts-service/accounts-service.service";
+import { AuthService } from "./modules/auth-service/auth-service.service";
+import { ClientsService } from "./modules/clients-service/clients-service.service";
+import { TransactionsService } from "./modules/transactions-service/transactions-service.service";
+
+export class PingResult {
+	@ApiProperty({ example: 23, description: "ping in ms" })
+	readonly ping: number;
+
+	@ApiProperty({ example: true })
+	readonly error: boolean;
+
+	@ApiProperty({ example: "auth-service sends hello!" })
+	readonly message: string;
+}
 
 @Injectable()
-export class AppService {}
+export class AppService {
+	constructor(
+		private readonly authService: AuthService,
+		private readonly clientsService: ClientsService,
+		private readonly accountsService: AccountsService,
+		private readonly transactionsService: TransactionsService
+	) {}
+
+	public async ping() {
+		const services = [
+			this.authService,
+			this.clientsService,
+			this.accountsService,
+			this.transactionsService,
+		];
+		const results: PingResult[] = [];
+
+		for (const service of services) {
+			const result = await this.pingOne(service);
+			results.push(result);
+		}
+
+		return results;
+	}
+
+	private async pingOne(service: { ping: (text: string) => Promise<string> }): Promise<PingResult> {
+		const now = Date.now();
+		let isError = false;
+		let message = "";
+
+		try {
+			message = await service.ping("hello!");
+		} catch (err) {
+			const error = err as Error;
+
+			isError = true;
+			message = error.message;
+		}
+
+		return {
+			ping: Date.now() - now,
+			error: isError,
+			message,
+		};
+	}
+}
