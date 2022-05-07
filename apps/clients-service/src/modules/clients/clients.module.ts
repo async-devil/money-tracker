be32f62eb2b053af553fe2257d5e966b831b1184
async-devil <async.devil@gmail.com>
2022-05-07 23:57:57 +0300

    fix(clients-s): fix model connection

--- apps/clients-service/src/modules/clients/clients.module.ts
+++ apps/clients-service/src/modules/clients/clients.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { Client } from "@nestjs/microservices";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { typeOrmConfigBase } from "../../database/ormconfig";
+import { Client } from "../../entities/client.entity";
 import { ClientsController } from "./clients.controller";
 import { ClientsRepository } from "./clients.repository";
 import { ClientsService } from "./clients.service";
