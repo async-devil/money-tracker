bbf83f0b6a2969c5dbf48f8c5e4fb488f6df3db7
async-devil <async.devil@gmail.com>
2022-05-07 23:59:27 +0300

    feat(clients-s): update entity to recent standart

--- apps/clients-service/src/database/migrations/1646602439167-AddClientEntity.ts
+++ apps/clients-service/src/database/migrations/1646602439167-AddClientEntity.ts
@@ -5,16 +5,18 @@ export class AddClientEntity1646602439167 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
-			`CREATE TABLE "client" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
-                "createDateTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), 
-                "lastChangedDateTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), 
-                "email" character varying NOT NULL, 
-                "password" character varying NOT NULL, 
-                
-                CONSTRAINT "UQ_6436cc6b79593760b9ef921ef12" UNIQUE ("email"), 
-                CONSTRAINT "PK_96da49381769303a6515a8785c7" PRIMARY KEY ("id")
-              )`
+			`
+			CREATE TABLE "client" (
+				"id"                     uuid NOT NULL DEFAULT uuid_generate_v4(),
+				"email"                  character varying NOT NULL, 
+        "password"               character varying NOT NULL, 
+
+				"create_date_time"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+				"last_changed_date_time" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+				CONSTRAINT "UQ_6436cc6b79593760b9ef921ef12" UNIQUE ("email"), 
+        CONSTRAINT "PK_96da49381769303a6515a8785c7" PRIMARY KEY ("id")
+			)`
 		);
 	}
 
--- apps/clients-service/src/entities/client.entity.ts
+++ apps/clients-service/src/entities/client.entity.ts
@@ -4,15 +4,11 @@ import { BaseEntity } from "./base.entity";
 
 @Entity({ name: "client" })
 export class Client extends BaseEntity {
-	/**
-	 * example: "test@email.com"
-	 */
+	/** @example "test@email.com"*/
 	@Column({ type: "varchar", unique: true, nullable: false })
 	email: string;
 
-	/**
-	 * example: "Password5@1"
-	 */
+	/** @example "Password5@1" */
 	@Column({ type: "varchar", nullable: false })
 	password: string;
 }
