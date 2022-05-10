2be631d1958c28afaf37b91af88c62405de96b10
async-devil <async.devil@gmail.com>
2022-05-11 00:01:18 +0300

    feat(gateway): add openApi property decorators

--- apps/gateway/src/modules/clients-service/types/request/update-client-by-id.dto.ts
+++ apps/gateway/src/modules/clients-service/types/request/update-client-by-id.dto.ts
@@ -1,14 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class ClientData {
 	/** @example "test@email.com" */
+	@ApiProperty({ example: "test@email.com" })
 	readonly email?: string;
 
 	/** @example "Password5@1" */
+	@ApiProperty({ example: "Password5@1" })
 	readonly password?: string;
 }
 
 export class UpdateClientByIdDto {
 	/** @example "54d45c35-f390-4a73-bf20-353ffffa2c42" */
+	@ApiProperty({ example: "54d45c35-f390-4a73-bf20-353ffffa2c42" })
 	readonly id: string;
 
+	@ApiProperty({ type: ClientData })
 	readonly data: ClientData;
 }
