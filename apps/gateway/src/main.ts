ef66bd0bec4fe33f4849f6adad94b8a628fc4660
async-devil <async.devil@gmail.com>
2022-05-27 00:02:52 +0300

    feat(gateway): init cookie parser

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
 
 import { AppModule } from "./app.module";
 import { ConfigService } from "./config/config.service";
@@ -8,12 +9,16 @@ import { ConfigService } from "./config/config.service";
 async function bootstrap() {
 	const config = new ConfigService();
 	const port = config.get<number>("port");
-	const swaggerConfig = new DocumentBuilder().setTitle("Nestjs template").setVersion("1.0.0").build();
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle("Nestjs template")
+		.setVersion("1.0.0")
+		.build();
 
 	const app = await NestFactory.create(AppModule);
 
-
 	app.setGlobalPrefix("api");
+
+	app.use(cookieParser());
 
 	const document = SwaggerModule.createDocument(app, swaggerConfig);
 	SwaggerModule.setup("/api/docs", app, document);
