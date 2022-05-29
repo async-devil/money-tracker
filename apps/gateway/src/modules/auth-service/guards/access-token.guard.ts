eb0cc7c7ac199fffb4faaba31dc89b8ef2d0977d
async-devil <async.devil@gmail.com>
2022-05-29 17:28:51 +0300

    feat(gateway): add access token guard docs

--- apps/gateway/src/modules/auth-service/guards/access-token.guard.ts
+++ apps/gateway/src/modules/auth-service/guards/access-token.guard.ts
@@ -3,6 +3,15 @@ import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from
 import { AuthService } from "../auth-service.service";
 import { IRequest } from "../types/interfaces/IRequest";
 
+/**
+ *	Access token guard that checks validity of the token and passes client id from it to request as clientId
+ *
+ * 	Throws:
+ * 	- { statusCode: 400, message: ["accessToken must be a jwt string"], error: "Bad request" }
+ * 	- { statusCode: 401, message: "No access token provided", error: "Unauthorized"}
+ * 	- { statusCode: 401, message: "Invalid access token", error: "Unauthorized"}
+ * 	- { statusCode: 500, message: "Unknown error", error: "Internal server error" }
+ */
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
 	constructor(private readonly authService: AuthService) {}
