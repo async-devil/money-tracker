d2f9decfc7fa48ac2c0803ae61d642c19c65047c
async-devil <async.devil@gmail.com>
2022-06-03 21:19:36 +0300

    feat(accounts-s): add accounts repository

--- apps/accounts-service/src/modules/accounts/accounts.repository.ts
+++ apps/accounts-service/src/modules/accounts/accounts.repository.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
@@ -12,5 +17,68 @@ export class AccountsRepository {
 
 	private throwDefaultError(message?: string) {
 		throw new InternalServerErrorException(message || "Internal Server Error");
+	}
+
+	public async getOneByProperty(property: { [key: string]: unknown }): Promise<Account> {
+		let account: Account;
+
+		try {
+			account = await this.accountsRepository.findOne({
+				where: property,
+			});
+		} catch (err) {
+			this.throwDefaultError();
+		}
+
+		if (!account) throw new NotFoundException("Account not found");
+		return account;
+	}
+
+	public async getManyByProperties(properties: { [key: string]: unknown }): Promise<Account[]> {
+		let accounts: Account[];
+
+		try {
+			accounts = await this.accountsRepository.find({
+				where: properties,
+			});
+		} catch (err) {
+			this.throwDefaultError();
+		}
+
+		return accounts;
+	}
+
+	public async save(account: Account): Promise<Account> {
+		try {
+			return await this.accountsRepository.save(account);
+		} catch (err) {
+			const error = err as Error;
+
+			if (error.message.includes("duplicate key value violates unique constraint")) {
+				throw new BadRequestException("Duplicate account");
+			}
+
+			this.throwDefaultError();
+		}
+	}
+
+	public async deleteOneByProperty(property: { [key: string]: unknown }) {
+		try {
+			await this.accountsRepository.delete(property);
+		} catch (err) {
+			this.throwDefaultError();
+		}
+
+		return {};
+	}
+
+	public async deleteAll(ownerId: string) {
+		try {
+			await this.accountsRepository.delete({ owner: ownerId });
+		} catch (err) {
+			this.throwDefaultError();
+		}
+
+		return {};
 	}
 }
