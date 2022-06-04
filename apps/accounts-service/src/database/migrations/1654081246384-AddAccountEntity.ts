589d03c83e7a99fdd0ba5eef837ad17d0f3d3922
async-devil <async.devil@gmail.com>
2022-06-04 13:42:54 +0300

    feat(accounts-s): update account entity

--- apps/accounts-service/src/database/migrations/1654081246384-AddAccountEntity.ts
+++ apps/accounts-service/src/database/migrations/1654081246384-AddAccountEntity.ts
@@ -5,27 +5,28 @@ export class AddAccountEntity1654081246384 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
-			`CREATE TYPE "account_type_enum" AS ENUM (
-				'regular', 
-				'savings', 
+			`
+			CREATE TYPE "account_type_enum" AS ENUM (
+				'regular',
+				'savings',
 				'debt'
 			)`
 		);
 		await queryRunner.query(
 			`
 			CREATE TABLE "account" (
-				"id"                     uuid NOT NULL DEFAULT uuid_generate_v4(), 
-				"owner"                  character varying NOT NULL, 
-				"type"                   "account_type_enum" NOT NULL, 
-				"name"                   character varying NOT NULL, 
-				"currency"               character varying NOT NULL, 
-				"balance"                numeric(18, 8) NOT NULL, 
-				"notes"                  character varying, 
-				"icon_name"              character varying NOT NULL DEFAULT 'MoreHoriz', 
-				"icon_color"             character varying NOT NULL DEFAULT 'A6A6A6', 
+				"id"                     uuid NOT NULL DEFAULT uuid_generate_v4(),
+				"owner"                  uuid NOT NULL,
+				"type"                   "account_type_enum" NOT NULL,
+				"name"                   character varying(50) NOT NULL, 
+				"currency"               character varying(3) NOT NULL, 
+				"balance"                numeric(18, 8) NOT NULL DEFAULT '0', 
+				"notes"                  character varying(200), 
+				"icon_name"              character varying(50) NOT NULL DEFAULT 'MoreHoriz', 
+				"icon_color"             character varying(6) NOT NULL DEFAULT 'A6A6A6', 
 
-				"create_date_time"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), 
-				"last_changed_date_time" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), 
+				"create_date_time"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+				"last_changed_date_time" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
 				CONSTRAINT "PK_54115ee388cdb6d86bb4bf5b2ea" PRIMARY KEY ("id")
 			)`
--- apps/accounts-service/src/entities/account.entity.ts
+++ apps/accounts-service/src/entities/account.entity.ts
@@ -11,40 +11,48 @@ export enum AccountType {
 @Entity({ name: "account" })
 export class Account extends BaseEntity {
 	/** @example "123e4567-e89b-12d3-a456-426655440000" */
-	@Column({ type: "varchar" })
+	@Column({ type: "uuid" })
 	public owner: string;
 
 	/** @example "regular" */
 	@Column({ type: "enum", enum: AccountType })
 	public type: AccountType;
 
-	/** @example "Personal card" */
-	@Column({ type: "varchar" })
+	/** Account name from 1 to 50 characters
+	 * @example "Personal card"
+	 */
+	@Column({ type: "varchar", length: 50 })
 	public name: string;
 
-	/** ISO 4217 currency code
+	/** ISO 4217 currency code. Could be any 3 uppercase characters
 	 * @example "USD"
 	 */
-	@Column({ type: "varchar" })
+	@Column({ type: "varchar", length: 3 })
 	public currency: string;
 
-	/** @example "1.35" */
-	@Column({ type: "numeric", precision: 18, scale: 8 })
+	/** Balance of the account. 18 numbers in total, 8 numbers after dot
+	 * @default 0
+	 * @example 11.35065001
+	 */
+	@Column({ type: "numeric", precision: 18, scale: 8, default: 0 })
 	public balance: number;
 
-	/** @example "My personal bank card" */
-	@Column({ type: "varchar", nullable: true })
+	/** Notes about account. From 1 to 200 characters
+	 * @optional
+	 * @example "My personal bank card"
+	 */
+	@Column({ type: "varchar", length: 200, nullable: true })
 	public notes?: string;
 
-	/** Name of icon for frontend
-	 * @example "MoreHoriz"
+	/** Name of icon for frontend. From 1 to 50 characters
+	 * @default "MoreHoriz"
 	 */
-	@Column({ type: "varchar", default: "MoreHoriz" })
+	@Column({ type: "varchar", length: 50, default: "MoreHoriz" })
 	public icon_name: string;
 
-	/** Hex icon color
-	 *  @example "A6A6A6"
+	/** Hex icon color for frontend. 6 characters without #
+	 * @default "A6A6A6"
 	 */
-	@Column({ type: "varchar", default: "A6A6A6" })
+	@Column({ type: "varchar", length: 6, default: "A6A6A6" })
 	public icon_color: string;
 }
