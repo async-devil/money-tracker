54ea30c18e2055365f9f4970fc56988f585e5733
async-devil <async.devil@gmail.com>
2022-06-28 09:36:25 +0300

    feat(auth-s): update session entity

--- apps/auth-service/src/database/migrations/1648394274350-AddSessionEntity.ts
+++ apps/auth-service/src/database/migrations/1648394274350-AddSessionEntity.ts
@@ -8,14 +8,11 @@ export class AddSessionEntity1648394274350 implements MigrationInterface {
 			`
 			CREATE TABLE "session" (
 				"id"                     uuid NOT NULL DEFAULT uuid_generate_v4(), 
-				"refresh_token"          character varying NOT NULL,
-				"client_id"              character varying NOT NULL,
+				"refresh_token"          character(48) NOT NULL,
+				"client_id"              uuid NOT NULL,
 				"valid_until"            TIMESTAMP WITH TIME ZONE NOT NULL, 
 				"ip"                     inet NOT NULL,
-				"device"                 character varying NOT NULL,
-
-				"create_date_time"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-				"last_changed_date_time" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+				"device"                 text NOT NULL,
 
 				CONSTRAINT "UQ_14f5d9fd42ee29c579807b5f7e5" UNIQUE ("refresh_token"),
 				CONSTRAINT "PK_f55da76ac1c3ac420f444d2ff11" PRIMARY KEY ("id")
--- apps/auth-service/src/entities/session.entity.ts
+++ apps/auth-service/src/entities/session.entity.ts
@@ -1,25 +1,30 @@
-import { Column, Entity } from "typeorm";
-
-import { BaseEntity } from "./base.entity";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: "session" })
-export class Session extends BaseEntity {
-	/** @example "b4d72d7e7ca77bc8221f81083fc24c5ec11f3dfe446bf521" */
-	@Column({ type: "varchar", unique: true })
-	refresh_token: string;
+export class Session {
+	/** @example "123e4567-e89b-12d3-a456-426655440000"*/
+	@PrimaryGeneratedColumn("uuid")
+	public id: string;
+
+	/** Session 48 chars long refresh token
+	 * @example "b4d72d7e7ca77bc8221f81083fc24c5ec11f3dfe446bf521"
+	 */
+	@Column({ type: "char", unique: true, length: 48 })
+	public refresh_token: string;
 
 	/** @example "123e4567-e89b-12d3-a456-426655440000" */
-	@Column({ type: "varchar" })
-	client_id: string;
+	@Column({ type: "uuid" })
+	public client_id: string;
 
+	/** @example "2022-06-27T06:34:59.882Z" */
 	@Column({ type: "timestamp with time zone" })
-	valid_until: Date;
+	public valid_until: Date;
 
 	/** @example "57.37.103.24" */
 	@Column({ type: "inet" })
-	ip: string;
+	public ip: string;
 
 	/** @example "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0)"*/
-	@Column({ type: "varchar" })
-	device: string;
+	@Column({ type: "text" })
+	public device: string;
 }
