0562cd8830d65566dc56dace669f69b1bf4bb959
async-devil <async.devil@gmail.com>
2022-07-09 19:40:16 +0300

    feat(frontend): add ant design

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,30 +1,20 @@
+import { Card, CardProps, Layout } from "antd";
 import React from "react";
 import styled from "styled-components";
 
 import logo from "./logo.svg";
 
-const AppBlock = styled.div`
+const CustomCard: React.FunctionComponent<CardProps> = styled(Card)`
 	text-align: center;
 
-	header {
-		background-color: #282c34;
-		min-height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		font-size: calc(10px + 2vmin);
-		color: white;
+	img {
+		height: 40vmin;
+		pointer-events: none;
+	}
 
+	@media (prefers-reduced-motion: no-preference) {
 		img {
-			height: 40vmin;
-			pointer-events: none;
-		}
-
-		@media (prefers-reduced-motion: no-preference) {
-			img {
-				animation: logo-spin infinite 20s linear;
-			}
+			animation: logo-spin infinite 20s linear;
 		}
 	}
 
@@ -44,8 +34,8 @@ const CustomLink = styled.a`
 
 function App() {
 	return (
-		<AppBlock>
-			<header>
+		<Layout>
+			<CustomCard title="Hello there!">
 				<img src={logo} alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
@@ -53,8 +43,8 @@ function App() {
 				<CustomLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
 				</CustomLink>
-			</header>
-		</AppBlock>
+			</CustomCard>
+		</Layout>
 	);
 }
 
--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "antd/dist/antd.dark.css";
+
 import "./normalize.css";
 import "./index.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
