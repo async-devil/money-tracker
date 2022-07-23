992ea6d9472385036a57a86eff312b8429e59080
async-devil <async.devil@gmail.com>
2022-07-23 22:23:16 +0300

    feat(frontend): add icons

--- apps/frontend/src/common/components/VariableIcon/iconsList.ts
+++ apps/frontend/src/common/components/VariableIcon/iconsList.ts
@@ -1,9 +1,21 @@
 import React from "react";
 import { IconType } from "react-icons";
-import { MdMoreHoriz } from "react-icons/md";
+import {
+	MdAccountBalance,
+	MdAccountBalanceWallet,
+	MdAttachMoney,
+	MdMoreHoriz,
+	MdOutlineShoppingBasket,
+	MdRestaurant,
+} from "react-icons/md";
 
 export const iconsList: { [key: string]: IconType } = {
 	MoreHoriz: MdMoreHoriz,
+	OutlineShoppingBasket: MdOutlineShoppingBasket,
+	AttachMoney: MdAttachMoney,
+	Restaurant: MdRestaurant,
+	AccountBalanceWallet: MdAccountBalanceWallet,
+	AccountBalance: MdAccountBalance
 };
 
 export default (iconName: string) => {
