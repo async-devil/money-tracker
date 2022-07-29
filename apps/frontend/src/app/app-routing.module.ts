fe1183490d2f6698b78ff014e0a5100e87531fde
async-devil <async.devil@gmail.com>
2022-07-29 22:05:10 +0300

    feat(frontend): add app module

---
+++ apps/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { DashboardPageComponent } from "./pages/dashboard/dashboard-page/dashboard-page.component";
+import { IndexPageComponent } from "./pages/index/index-page/index-page.component";
+import { TransactionsPageComponent } from "./pages/transactions/transactions-page/transactions-page.component";
+import { AuthGuard } from "./shared/guards/auth/auth.guard";
+
+const routes: Routes = [
+	{ path: "", component: IndexPageComponent },
+	{
+		path: "dashboard",
+		component: DashboardPageComponent,
+		canActivate: [AuthGuard],
+	},
+	{
+		path: "dashboard/transactions",
+		component: TransactionsPageComponent,
+		canActivate: [AuthGuard],
+	},
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ apps/frontend/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AccountsModule } from "./pages/accounts/accounts.module";
+import { DashboardModule } from "./pages/dashboard/dashboard.module";
+import { IndexModule } from "./pages/index/index.module";
+import { TransactionsModule } from "./pages/transactions/transactions.module";
+import { AuthInterceptor } from "./shared/interceptors/auth.interceptor";
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		DashboardModule,
+		IndexModule,
+		TransactionsModule,
+		AccountsModule,
+	],
+	providers: [
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true,
+		},
+	],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
