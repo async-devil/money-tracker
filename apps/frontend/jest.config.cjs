c361b454eed6e88b5c6f9b4f3fcc91220eb15191
async-devil <async.devil@gmail.com>
2022-07-30 13:12:42 +0300

    test(frontend): update jest config

--- apps/frontend/jest.config.cjs
+++ apps/frontend/jest.config.cjs
@@ -1,15 +1,14 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig");
-
 module.exports = {
-	preset: "jest-preset-angular",
-	roots: ["<rootDir>/src/"],
-	testMatch: ["**/+(*.)+(spec).+(ts)"],
+	moduleFileExtensions: ["js", "json", "ts"],
+	moduleNameMapper: {
+		"^src/(.*)$": "<rootDir>/src/$1",
+	},
+	rootDir: "./",
+	testRegex: ".*\\.spec\\.ts$",
 	setupFilesAfterEnv: ["<rootDir>/src/test.ts"],
-	collectCoverage: true,
-	coverageReporters: ["html"],
-	coverageDirectory: "coverage/my-app",
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-		prefix: "<rootDir>/",
-	}),
+	collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s"],
+	coverageDirectory: "./coverage",
+
+	preset: "jest-preset-angular",
+	testEnvironment: "jsdom",
 };
