520f7104357517bfd5bf634c991f9a0f535e9655
async-devil <async.devil@gmail.com>
2022-08-10 22:11:07 +0300

    feat(frontend): add account component

---
+++ apps/frontend/src/app/modules/dashboard/components/account/account.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+import { Account } from "src/app/services/accounts/types/response/account.entity";
+
+@Component({
+	selector: "app-account",
+	templateUrl: "./account.component.html",
+	styleUrls: ["./account.component.scss"],
+})
+export class AccountComponent {
+	@Input() account: Account;
+
+	@Input() onClick: (account: Account) => unknown;
+}
