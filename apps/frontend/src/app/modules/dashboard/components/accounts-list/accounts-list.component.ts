36cb42ad04cc40425dba1b38bef97d5ad464809f
async-devil <async.devil@gmail.com>
2022-08-11 22:17:56 +0300

    feat(frontend): add accounts-list component

---
+++ apps/frontend/src/app/modules/dashboard/components/accounts-list/accounts-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+import { Account } from "src/app/services/accounts/types/response/account.entity";
+
+@Component({
+	selector: "app-accounts-list",
+	templateUrl: "./accounts-list.component.html",
+	styleUrls: ["./accounts-list.component.scss"],
+})
+export class AccountsListComponent {
+	@Input() accounts: Array<[string, Array<Account>]>;
+}
--- apps/frontend/src/app/modules/dashboard/dashboard.module.ts
+++ apps/frontend/src/app/modules/dashboard/dashboard.module.ts
@@ -3,27 +3,35 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 import { SetAmountColorPipe } from "src/app/shared/pipes/set-amount-color/set-amount-color.pipe";
+import { SetBalanceColorPipe } from "src/app/shared/pipes/set-balance-color/set-balance-color.pipe";
 import { TransformAmountPipe } from "src/app/shared/pipes/transform-amount/transform-amount.pipe";
 import { SharedModule } from "src/app/shared/shared.module";
 
 import { MuiModule } from "../mui.module";
+import { AccountComponent } from "./components/account/account.component";
 import { TransactionComponent } from "./components/transaction/transaction.component";
+import { TransactionsListComponent } from "./components/transactions-list/transactions-list.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { AccountsPageComponent } from "./pages/accounts/accounts-page.component";
 import { DashboardPageComponent } from "./pages/dashboard/dashboard-page.component";
 import { DashboardModuleComponent } from "./pages/index/dashboard-module.component";
 import { TransactionsPageComponent } from "./pages/transactions/transactions-page.component";
+import { AccountsListComponent } from './components/accounts-list/accounts-list.component';
 
 @NgModule({
 	declarations: [
 		SetAmountColorPipe,
+		SetBalanceColorPipe,
 		TransformAmountPipe,
-		TransactionComponent,
 
+		TransactionComponent,
 		DashboardModuleComponent,
 		DashboardPageComponent,
 		AccountsPageComponent,
 		TransactionsPageComponent,
+		AccountComponent,
+		TransactionsListComponent,
+  AccountsListComponent,
 	],
 	imports: [CommonModule, RouterModule, DashboardRoutingModule, SharedModule, MuiModule],
 })
