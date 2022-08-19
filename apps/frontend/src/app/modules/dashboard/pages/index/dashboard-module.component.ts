e04966f3565cb6a8c394496250a6282fd2ccd311
async-devil <async.devil@gmail.com>
2022-08-19 12:02:17 +0300

    feat(frontend): update dashboard menu

--- apps/frontend/src/app/modules/dashboard/pages/index/dashboard-module.component.ts
+++ apps/frontend/src/app/modules/dashboard/pages/index/dashboard-module.component.ts
@@ -1,8 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NavigationStart, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
 	selector: "app-dashboard-module",
 	templateUrl: "./dashboard-module.component.html",
 	styleUrls: ["./dashboard-module.component.scss"],
 })
-export class DashboardModuleComponent {}
+export class DashboardModuleComponent implements OnInit {
+	public currentPage: string;
+
+	constructor(private readonly router: Router) {}
+
+	public ngOnInit() {
+		this.router.events
+			.pipe(filter((event) => event instanceof NavigationStart))
+			.subscribe((event) => {
+				const data = event as NavigationStart;
+
+				const match = data.url.match(/(?<=\/dashboard\/)\w*/);
+
+				this.currentPage = match ? match[0] : "dashboard";
+			});
+	}
+}
