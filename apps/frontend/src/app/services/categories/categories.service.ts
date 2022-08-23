90a7326fb41c95b0c5fbb11b247ce11dda149d12
async-devil <async.devil@gmail.com>
2022-08-23 21:38:55 +0300

    feat(frontend): add update and create methods to categories service

--- apps/frontend/src/app/services/categories/categories.service.ts
+++ apps/frontend/src/app/services/categories/categories.service.ts
@@ -4,8 +4,10 @@ import { catchError, Observable, throwError } from "rxjs";
 
 import { ErrorService } from "../error/error.service";
 import { HttpException } from "../error/types/HttpException";
+import { CreateCategoryDto } from "./types/request/create-category.dto";
 import { GetCategoriesByQueryDto } from "./types/request/get-categories-by-properties.dto";
 import { GetCategoryByIdDto } from "./types/request/get-category-by-id.dto";
+import { UpdateCategoryByIdDto } from "./types/request/update-category-by-id.dto";
 import { Category } from "./types/response/category.entity";
 
 @Injectable({
@@ -31,6 +33,18 @@ export class CategoriesService {
 
 	public getById(dto: GetCategoryByIdDto) {
 		return this.http.get<Category>(`/api/categories/${dto.id}`);
+	}
+
+	public create(dto: CreateCategoryDto): Observable<Category> {
+		return this.http
+			.post<Category>("/api/categories", dto)
+			.pipe(catchError((err: HttpErrorResponse) => this.errorHandler(err)));
+	}
+
+	public updateById(dto: UpdateCategoryByIdDto): Observable<Category> {
+		return this.http
+			.put<Category>(`/api/categories/${dto.id}`, dto.data)
+			.pipe(catchError((err: HttpErrorResponse) => this.errorHandler(err)));
 	}
 
 	private errorHandler(error: HttpErrorResponse) {
