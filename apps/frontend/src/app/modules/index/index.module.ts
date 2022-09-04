d22ae6229627eac9b9af7a25324f2d9bc7656be3
async-devil <async.devil@gmail.com>
2022-09-04 13:47:16 +0300

    feat(frontend): update index page

--- apps/frontend/src/app/modules/index/index.module.ts
+++ apps/frontend/src/app/modules/index/index.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import { MuiModule } from "../mui.module";
 import { IndexRoutingModule } from "./index-routing.module";
 import { HomePageComponent } from "./pages/home/home-page.component";
 
 @NgModule({
 	declarations: [HomePageComponent],
-	imports: [CommonModule, RouterModule, IndexRoutingModule],
+	imports: [CommonModule, RouterModule, IndexRoutingModule, MuiModule],
 })
 export class IndexModule {}
